ea7aba127c65b3f58f063a924c60454e714c19a8
kennyinuzuka <ti21@estudiosul.com.br>
2022-06-01 20:37:51 -0300

    comentario

---
+++ index.php
@@ -0,0 +1,33 @@
+<style>
+    body{
+        height:100vh;
+        width: 100vw;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
+        background: crimson;
+    }
+
+    h1{
+        font-family: sans-serif;
+        font-weight: bold;
+        font-size: 3rem;
+        color:white;
+        animation: testeloho 5s infinite;
+    }
+
+    @keyframes testeloho {
+        0%{
+            transform: scale(1);
+        }
+        75%{
+            transform: scale(1.7);
+        }
+    }
+</style>
+
+
+
+
+<h1>Olá mundo</h1>
